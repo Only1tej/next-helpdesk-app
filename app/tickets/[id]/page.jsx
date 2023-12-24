9ae9143cf5a34e43a7dfa21378f84aba79616c6e
only1tej <tijanienny3@gmail.com>
2023-12-24 12:18:29 +0100

    commit

---
+++ app/tickets/[id]/page.jsx
@@ -0,0 +1,51 @@
+import Link from "next/link"
+import { notFound } from "next/navigation"
+
+export const dynamicParams = true
+
+export async function generateStaticParams() {
+    //[{id: 1}, {id: 2}, ...]
+    const res = await fetch('http://localhost:4000/tickets')
+
+    const tickets = await res.json()
+
+    return tickets.map((ticket) => ({
+        id: ticket.id
+    }))
+}
+
+async function getTicket(id) {
+    //imitate delay
+    await new Promise(resolve => setTimeout(() => resolve(), 3000))
+
+    const res = await fetch('http://localhost:4000/tickets/' + id, {
+        next: {
+            revalidate: 60
+        }
+    })
+
+    if (!res.ok) {
+        notFound()
+    }
+
+    return res.json()
+}
+
+export default async function TicketDetails({ params }) {
+    const ticket = await getTicket(params.id)
+    return (
+        <main>
+            <nav>
+                <h2>Ticket Details</h2>
+                <Link href={'/tickets'} className="ml-[760px] px-2 py-1 rounded-xl bg-violet-500 text-white">Back</Link>
+            </nav>
+            <div className="card">
+                <h3>{ticket.title}</h3>
+                <small>Created by: {ticket.user_email}</small>
+                <p>{ticket.body}</p>
+                <div className={`pill ${ticket.priority}`}>
+                    {ticket.priority} priority</div>
+            </div>
+        </main>
+    )
+}
---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CreateForm from './CreateForm'
+
+export default function CreateTicket() {
+    return (
+        <main>
+            <h2 className="text-primary text-center">Add a New Tciket</h2>
+            <CreateForm />
+        </main>)
+}
